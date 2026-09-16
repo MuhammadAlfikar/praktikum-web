b28216f5aa039397a1fed181ff5d6bb0e6f287dc
Fikar <malfikar181@gmail.com>
2026-09-16 23:29:06 +0700

    Pertemuan 2 latihan PHP dasar

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+Route::get('/latihan-php', function () {
+    $nama = 'Muhammad Alfikar';
+    $nilai = [80, 85, 75, 90, 88];
+
+    $hitungRataRata = function (array $data): float {
+        $total = 0;
+
+        foreach ($data as $angka) {
+            $total += $angka;
+        }
+
+        return $total / count($data);
+    };
+
+    $rataRata = $hitungRataRata($nilai);
+
+    if ($rataRata >= 75) {
+        $status = 'Lulus';
+    } else {
+        $status = 'Perlu Perbaikan';
+    }
+
+    return view('latihan-php', compact(
+        'nama',
+        'nilai',
+        'rataRata',
+        'status'
+    ));
+});
